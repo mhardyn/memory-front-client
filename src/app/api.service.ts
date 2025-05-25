c9fef986b76beda3c5c9421c62404b5f6ea02ec0
mhardyn <hardynmagda@gmail.com>
2025-05-25 15:16:55 +0200

    API Service - fiszki

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Category} from './word-bank/models';
+import {Card, Category} from './word-bank/models';
 
 const BASE_URL = 'http://localhost:5000'
 @Injectable({
@@ -22,5 +22,20 @@ export class ApiService {
     return this.httpClient.post<Category>(`${BASE_URL}/categories`, {
       category_name: categoryName
     })
+  }
+
+  getCards(categoryId: number): Observable<Card[]> {
+    return this.httpClient.get<Card[]>(`${BASE_URL}/categories/${categoryId}/cards`);
+  }
+
+  addCard(categoryId: number, word: string, definition: string): Observable<Card> {
+    return this.httpClient.post<Card>(`${BASE_URL}/categories/${categoryId}/cards`, {
+      word,
+      definition
+    })
+  }
+
+  removeCard(categoryId: number, cardId: number): Observable<any> {
+    return this.httpClient.delete(`${BASE_URL}/categories/${categoryId}/cards/${cardId}`);
   }
 }
--- src/app/word-bank/models.ts
+++ src/app/word-bank/models.ts
@@ -3,3 +3,9 @@ export interface Category {
   category_name: string;
   cards_count: number;
 }
+export interface Card {
+  category_id: number;
+  card_id: number;
+  word: string;
+  definition: string;
+}
