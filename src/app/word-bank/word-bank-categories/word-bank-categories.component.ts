f0e86485ebe6990b83204140abbb7b460123b83e
mhardyn <hardynmagda@gmail.com>
2025-05-25 12:08:48 +0200

    Lista kategorii - wypełniona lista

--- src/app/word-bank/word-bank-categories/word-bank-categories.component.ts
+++ src/app/word-bank/word-bank-categories/word-bank-categories.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
+import {Category} from '../models';
 
 @Component({
   selector: 'app-word-bank-categories',
   imports: [
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './word-bank-categories.component.html',
   styleUrl: './word-bank-categories.component.css',
   standalone: true
 })
 export class WordBankCategoriesComponent {
-  categories =[];
+  categories: Category[]=[
+    {category_id: 1, category_name: "Sport", cards_count: 1},
+    {category_id: 2, category_name: "Rzeczowniki", cards_count: 0}
+  ];
 }
